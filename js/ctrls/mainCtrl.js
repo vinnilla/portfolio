30add099efb5400ef6accd999f9ff09e955676f4
Vincent <vincentckhuang@gmail.com>
2016-09-10 19:52:15 -0700

    converted to flex display

--- js/ctrls/mainCtrl.js
+++ js/ctrls/mainCtrl.js
@@ -23,13 +23,19 @@
 	function home(factory) {
 		var self = this;
 		this.factory = factory
-
+		this.words = ['Full', 'Stack', 'Web', 'Developer']
 	}// end of home function
 
 	function skills(factory) {
 		var self = this;
 		this.factory = factory
-
+		this.languages = [
+			['devicon-html5-plain', 'devicon-css3-plain', 'devicon-javascript-plain', 'devicon-ruby-plain', 'devicon-cplusplus-plain'],
+			['devicon-jquery-plain', 'devicon-angularjs-plain'],
+			['devicon-nodejs-plain', 'devicon-rails-plain'],
+			['devicon-postgresql-plain', 'devicon-mongodb-plain'],
+			['devicon-git-plain', 'devicon-github-plain']
+		]
 	}// end of skills function
 
 	function projects(factory) {
